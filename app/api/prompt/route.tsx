ec30d204ff92444efc4ee98d0b90ace6953c91f1
lam-tu-tr <lam.tu.tr@gmail.com>
2023-06-16 22:27:45 -0700

    Removed default from prompt POST

--- app/api/prompt/route.tsx
+++ app/api/prompt/route.tsx
@@ -20,7 +20,7 @@ export const runtime = "edge";
 //    gets added to prompt by user and system
 //-----------------------------------------------------------------------------
 
-export default async function POST(req: NextRequest): Promise<NextResponse> {
+export async function POST(req: NextRequest): Promise<NextResponse> {
   console.log("inside Post Function");
   const prompt: promptType = await req.json();
   if (!prompt) {
