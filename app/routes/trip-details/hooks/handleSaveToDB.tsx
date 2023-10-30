cd9012b0e6fc1aef64a7275127976fffb42e4a49
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-10-29 21:06:04 -0700

    Separated detail page save to db to its own file

---
+++ app/routes/trip-details/hooks/handleSaveToDB.tsx
@@ -0,0 +1,68 @@
+//*Handle Save to db
+
+import { toastError, toastSuccess } from "@/helpers/toast";
+import { destType } from "@/helpers/types";
+import router from "next/router";
+
+async function copyToClipboard(tripId: string) {
+  try {
+    await navigator.clipboard.writeText(
+      `${window.location.href}/routes/tripId?tripId=${tripId}`
+    );
+  } catch (err) {
+    toastError("Failed to copy to clipboard, please try again");
+  }
+}
+
+// type SaveToDBProptype = {
+//   type: string;
+//   currentUser: string;
+//   dest: destType;
+// };
+
+export default async function handleSaveToDB(
+  type: string,
+  dest: destType,
+  currentUser: string | null
+) {
+  if (currentUser == null) {
+    toastError("Please log in to enable trip saving and sharing");
+    return;
+  }
+
+  try {
+    const res = await fetch("../../../api/trip", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        dbPayload: {
+          username: currentUser,
+          destName: dest.destName,
+          aiMessage: dest.aiMessage,
+          destList: dest.destList,
+          bbox: dest.bbox,
+          startDate: dest.startDate,
+          endDate: dest.endDate,
+        },
+        type: "create",
+      }),
+    });
+
+    if (!res.ok) throw new Error("Failed to save to account");
+
+    const { tripId } = await res.json();
+
+    copyToClipboard(tripId);
+    if (type == "save") {
+      toastSuccess("Trip saved to account and clipboard");
+      // router.push(`/routes/tripId?tripId=${tripId}`);
+    } else {
+      //TODO separate copy to clipboard, if signed in, look for the unique url from database, otherwise, copy the stringparams url.
+      toastSuccess("Copied to clipboard");
+    }
+  } catch (err) {
+    toastError("Couldnt copy to clipboard");
+  }
+}
--- app/routes/trip-details/page.tsx
+++ app/routes/trip-details/page.tsx
@@ -4,11 +4,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
-import { capitalizeWords } from "../../helpers/helper-functions";
-import { Message, destType } from "../../helpers/types";
+import { capitalizeWords } from "@/helpers/helper-functions";
+import { Message, destType } from "@/helpers/types";
 import Map from "@/components/Map/Map";
 
 import setInitialPrompt from "./hooks/setInitialPrompt";
+import handleSaveToDB from "./hooks/handleSaveToDB";
 
 // import { useGlobalContext } from "@/Context";
 import { FiArrowUpCircle, FiSave, FiCopy } from "react-icons/fi";
@@ -29,7 +30,7 @@ const DOMPurifyConfig = {
 export default function Trip() {
   //*================================================================================
   //*States declarations */
-
+  const router = useRouter();
   const [currentUser, setCurrentUser] = useState<string | null>(null);
 
   useEffect(() => {
@@ -60,8 +61,6 @@ export default function Trip() {
 
   const [currDest, setCurrDest] = useState<[number, number]>();
 
-  const router = useRouter();
-
   //*handle submit, assign messages to payload
   function handleConvo(e: any) {
     //!reset these variables for each time user submits adjustments
@@ -79,60 +78,6 @@ export default function Trip() {
       { role: "assistant", content: dest.aiMessage },
       { role: "user", content: userMessage },
     ]);
-  }
-
-  //*Handle Save to db
-  async function handleSaveToDB(type: string) {
-    if (currentUser == null) {
-      toastError("Please log in to enable trip saving and sharing");
-      return;
-    }
-
-    try {
-      const res = await fetch("../../api/trip", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          dbPayload: {
-            username: currentUser,
-            destName: dest.destName,
-            aiMessage: dest.aiMessage,
-            destList: dest.destList,
-            bbox: dest.bbox,
-            startDate: dest.startDate,
-            endDate: dest.endDate,
-          },
-          type: "create",
-        }),
-      });
-
-      if (!res.ok) throw new Error("Failed to save to account");
-
-      const { tripId } = await res.json();
-
-      copyToClipboard(tripId);
-      if (type == "save") {
-        toastSuccess("Trip saved to account and clipboard");
-        router.push(`/routes/tripId?tripId=${tripId}`);
-      } else {
-        toastSuccess("Copied to clipboard");
-      }
-    } catch (err) {
-      toastError("Couldnt copy to clipboard");
-    }
-  }
-
-  async function copyToClipboard(tripId: string) {
-    try {
-      await navigator.clipboard.writeText(
-        `${window.location.href}/routes/tripId?tripId=${tripId}`
-      );
-    } catch (err) {
-      toastError("Failed to copy to clipboard");
-      console.log("Clipboard save error", err);
-    }
   }
 
   useFetchLocation(aiComplete, setDest, dest.bbox);
@@ -207,7 +152,7 @@ export default function Trip() {
         <div id="h1_wrapper">
           <button
             title="Copy Trip Link"
-            onClick={() => handleSaveToDB("")}
+            onClick={() => handleSaveToDB("", dest, currentUser)}
             type="button"
           >
             <FiCopy className="w-6 h-6 m-4" />
@@ -216,7 +161,7 @@ export default function Trip() {
 
           <button
             title="Save to Account"
-            onClick={() => handleSaveToDB("save")}
+            onClick={() => handleSaveToDB("save", dest, currentUser)}
             type="button"
           >
             <FiSave className="w-6 h-6 m-4" />
