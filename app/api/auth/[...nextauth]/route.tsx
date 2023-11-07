c590e95cfc1aa4b0531469800b5f4e1dd8d11d71
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-11-06 22:01:04 -0800

    Setting up authentication page

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -8,6 +8,9 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  // pages: {
+  //   signIn: "./SignIn",
+  // },
 });
 
 export { handler as GET, handler as POST };
