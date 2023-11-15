03b78b992ad4b666cc7ca48c4aecc4e5f05c0514
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-11-14 21:49:28 -0800

    Commented out unused account code

--- app/routes/account/hooks/useGetLocationList.tsx
+++ app/routes/account/hooks/useGetLocationList.tsx
@@ -1,37 +1,37 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-export default function useGetLocationList() {
-  useEffect(() => {
-    async function initVars() {
-      setDestItems([]);
-      try {
-        const userFromStorage =
-          isWindow && window.sessionStorage.getItem("currentUser");
-        if (userFromStorage) {
-          const res = await fetch("../../api/trip", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              dbPayload: userFromStorage,
-              type: "getAllList",
-            }),
-          });
+// export default function useGetLocationList() {
+//   useEffect(() => {
+//     async function initVars() {
+//       setDestItems([]);
+//       try {
+//         const userFromStorage =
+//           isWindow && window.sessionStorage.getItem("currentUser");
+//         if (userFromStorage) {
+//           const res = await fetch("../../api/trip", {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//               dbPayload: userFromStorage,
+//               type: "getAllList",
+//             }),
+//           });
 
-          if (!res.ok) throw new Error("Failed to Init Variables");
+//           if (!res.ok) throw new Error("Failed to Init Variables");
 
-          const { tripInfo } = await res.json();
-          setDestItems(tripInfo);
+//           const { tripInfo } = await res.json();
+//           setDestItems(tripInfo);
 
-          console.log("finished data transfer");
-        }
-      } catch (err) {
-        toastError("Couldnt transfer data to server");
-      }
-    }
+//           console.log("finished data transfer");
+//         }
+//       } catch (err) {
+//         toastError("Couldnt transfer data to server");
+//       }
+//     }
 
-    initVars();
-  }, [isWindow, currentUser]);
-  return <div>useGetLocationList</div>;
-}
+//     initVars();
+//   }, [isWindow, currentUser]);
+//   return <div>useGetLocationList</div>;
+// }
