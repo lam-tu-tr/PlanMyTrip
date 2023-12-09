7e30e7967c56f5cdfcd856bf007f40fe3218fab2
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-12-08 22:07:27 -0800

    Reformatted pregenerated trip route

--- app/routes/[trip]/page.tsx
+++ app/routes/[trip]/page.tsx
@@ -1,27 +1,20 @@
 //*
 //*--------------------------/tripId?tripId=________________------------------------------------
-
 "use client";
 import React, { useEffect, useState } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
-import { capitalizeWords } from "../../helpers/helper-functions";
+import { useSearchParams } from "next/navigation";
 
 import Map from "@/components/Map/Map";
 
 import { destType } from "@/helpers/types";
 
-import DOMPurify from "isomorphic-dompurify";
-import { toastError } from "@/helpers/toast";
-import { useRouter } from "next/navigation";
 import useFetchTripInfo from "./hooks/useFetchTripInfo";
 import Itinerary from "@/components/Itinerary/Itinerary";
 
 import AiChatBox from "@/components/AiChatBox/AiChatBox";
 
 import "../trip-details/trip-details.scss";
-const DOMPurifyConfig = {
-  ADD_ATTR: ["target"], //*allow target attribute on anchor tags to go through
-};
+import useHandleLocationHover from "../trip-details/hooks/useHandleLocationHover";
 
 type DestCoordType = {
   [key: string]: [longitude: number, latitude: number];
@@ -44,42 +37,14 @@ export default function Trip() {
   useFetchTripInfo({ tripId, setDest });
 
   const [currDest, setCurrDest] = useState<[number, number]>();
-
-  // useEffect(() => {
-  //   function handleLocHover(event: any) {
-  //     setCurrDest(dest.destList[event.target.innerText]);
-  //   }
-  //   const chatSection = document.querySelector(".chat");
-
-  //   const handleHoverEvent = (event: any) => {
-  //     if (event.target.classList.contains("ai-location")) {
-  //       handleLocHover(event);
-  //     }
-  //   };
-  //   chatSection!.addEventListener("mouseover", handleHoverEvent);
-
-  //   return () => {
-  //     chatSection!.removeEventListener("mouseover", handleHoverEvent);
-  //   };
-  // }, [dest.destList]);
-
-  // //*Auto scrolling to bottom as aiMessage text generates
-  // useEffect(() => {
-  //   const textarea = document.getElementById("chat");
-  //   textarea!.scrollTop = textarea!.scrollHeight;
-  // }, []);
+  useHandleLocationHover(dest.destList, setCurrDest);
 
   return (
     <div className="TripDetails page-container">
       <Map currDest={currDest} dest={dest} setDest={setDest} />
-      <form className="trip_form">
+      <section className="itinerary_container">
         <Itinerary dest={dest} />
-        {/* <AiChatBox
-          userMessage={userMessage}
-          aiComplete={aiComplete}
-          setUserMessage={setUserMessage}
-        /> */}
-      </form>
+      </section>
     </div>
   );
 }
