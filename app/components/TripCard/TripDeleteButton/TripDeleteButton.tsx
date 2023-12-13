0f1b6576d5a437ccd33f2a469f0a74c0736fca5d
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-12-13 14:49:28 -0800

    Working TripDeleteButton ssr refresh after deleting on server

--- app/components/TripCard/TripDeleteButton/TripDeleteButton.tsx
+++ app/components/TripCard/TripDeleteButton/TripDeleteButton.tsx
@@ -2,16 +2,19 @@
 import React from "react";
 import { handleDeleteDestination } from "./handleDeleteDestination/handleDeleteDestination";
 import { TiDelete } from "react-icons/ti";
+import { useRouter } from "next/navigation";
 
 type TripDeleteButtonProp = {
   trip_id: string;
 };
 
 export function TripDeleteButton({ trip_id }: TripDeleteButtonProp) {
+  const router = useRouter();
   return (
     <button
       onClick={() => {
         handleDeleteDestination(trip_id);
+        router.refresh();
       }}
     >
       <TiDelete className="w-8 h-8" />
