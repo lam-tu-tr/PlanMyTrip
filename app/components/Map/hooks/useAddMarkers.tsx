c987e89d4d8c704c2fb2ad6f67e0bd4841c18791
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-12-19 12:20:35 -0800

    Revised map markers to have emoji in center

--- app/components/Map/hooks/useAddMarkers.tsx
+++ app/components/Map/hooks/useAddMarkers.tsx
@@ -30,19 +30,15 @@ export function useAddMarkers({
       //*Loop through locations and set a marker for each destination, and pushing
       //*marker into markers array
       Object.values(locations).forEach((date) => {
+        const date_color = getRandomColor();
+
         Object.entries(date).forEach(([location, info], index) => {
-          setMarkers((prev: Marker[]) => [
-            ...prev,
-            new mapboxgl.Marker({
-              color: `#${Math.random()
-                .toString(16)
-                .slice(2, 8)
-                .padStart(6, "0")}`,
-            })
-              .setLngLat(info.coordinate)
-              .setPopup(new mapboxgl.Popup().setHTML(`${location}`))
-              .addTo(map),
-          ]);
+          const { marker } = createMarkerAndStyle(info.emoji, date_color);
+
+          new mapboxgl.Marker(marker)
+            .setLngLat(info.coordinate)
+            .setPopup(new mapboxgl.Popup().setHTML(`${location}`))
+            .addTo(map);
         });
       });
     } else if (
@@ -56,4 +52,29 @@ export function useAddMarkers({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [locations, mapboxgl.LngLatBounds, mapboxgl.Marker, mapboxgl.Popup]);
+}
+
+function getRandomColor() {
+  // return `#${Math.random().toString(16).slice(2, 8).padStart(6, "0")}`;
+  // Generate a random hue value
+  const hue = Math.floor(Math.random() * 360);
+
+  // Set lower saturation and brightness for muted colors
+  const saturation = Math.floor(Math.random() * 30) + 20; // Adjust the range as needed
+  const lightness = Math.floor(Math.random() * 30) + 50; // Adjust the range as needed
+
+  // Convert HSL to RGB
+  const rgbColor = `hsl(${hue},${saturation}%,${lightness}%)`;
+
+  return rgbColor;
+}
+
+function createMarkerAndStyle(emoji: string, color: string) {
+  const marker = document.createElement("div");
+
+  marker.className = "marker";
+  marker.style.backgroundColor = color;
+  marker.textContent = emoji;
+
+  return { marker };
 }
